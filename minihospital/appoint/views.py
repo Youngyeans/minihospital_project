a9bdda8005621960b642439402f3ed677f1dd7d0
youngyeans <nattanicha.suw@gmail.com>
2024-10-06 22:41:31 +0700

    save appoint-today

--- minihospital/appoint/views.py
+++ minihospital/appoint/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from datetime import date
 
 class AppointTodayView(View):
 
     def get(self, request):
-        doctor_list = range(5)
-        return render(request, 'appointment-today.html', {'doctor_list': doctor_list})
+        doctor_list = range(9)
+        today = date.today() 
+        context ={
+            'doctor_list': doctor_list,
+            'today': today,
+        }
+        return render(request, 'appointment-today.html', context)
     
 class DoctorListView(View):
 
---
+++ minihospital/static/js/categories.js
@@ -0,0 +1,40 @@
+const count = 6;
+
+    // วนลูปตามจำนวนที่ต้องการ
+    for (let i = 1; i <= count; i++) {
+        const hoverArea = document.getElementById("cat" + i);
+        const targetImage = document.getElementById("img" + i);
+
+        // ตรวจสอบว่า hoverArea และ targetImage มีอยู่ก่อนที่จะเพิ่ม event listener
+        if (hoverArea && targetImage) {
+            // เพิ่ม transition ให้ภาพตั้งแต่แรก
+            targetImage.classList.add('transition', 'duration-300', 'ease-in-out', 'delay-150');
+
+            // เมื่อเมาส์เข้าไปใน hover-area
+            hoverArea.addEventListener('mouseenter', function() {
+                targetImage.classList.add('brightness-200');  // เพิ่มคลาส brightness-200
+            });
+
+            // เมื่อเมาส์ออกจาก hover-area
+            hoverArea.addEventListener('mouseleave', function() {
+                targetImage.classList.remove('brightness-200');  // ลบคลาส brightness-200
+            });
+        }
+    }
+
+    const border = document.getElementById('search1');
+    const bg = document.getElementById('search2');
+
+    border.addEventListener('mouseenter', function() {
+        bg.classList.add('bg-[#15CDCB]');
+        bg.classList.add('text-white');
+        bg.classList.remove('bg-white');
+        bg.classList.remove('text-[#494949]');
+    });
+
+    border.addEventListener('mouseleave', function() {
+        bg.classList.remove('bg-[#15CDCB]');
+        bg.classList.remove('text-white');
+        bg.classList.add('bg-white');
+        bg.classList.add('text-[#494949]');
+    });
